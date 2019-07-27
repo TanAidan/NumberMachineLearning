51948593444c24148cdea8029503122f45d0937d
League Student <league@iMac-24.attlocal.net>
2019-07-27 12:58:21 -0700

    got require to work, finish copy pasting the test data and traininga and predicting model next time

--- NumberML/WebContent/scripts/VideoLoader.js
+++ NumberML/WebContent/scripts/VideoLoader.js
@@ -1,14 +1,51 @@
 
 //copy video into a canvas, read intensities of pixels, and downsample the image (0-255 gray/5x5)
 //lib svm
-/*//AIDAN'S CODE
+//AIDAN'S CODE
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	///- REQUIRE FN
+	// equivalent to require from node.js
+	function require(url){
+	    if (url.toLowerCase().substr(-3)!=='.js') url+='.js'; // to allow loading without js suffix;
+	    if (!require.cache) require.cache=[]; //init cache
+	    var exports=require.cache[url]; //get from cache
+	    if (!exports) { //not cached
+	            try {
+	                exports={};
+	                var X=new XMLHttpRequest();
+	                X.open("GET", url, 0); // sync
+	                X.send();
+	                if (X.status && X.status !== 200)  throw new Error(X.statusText);
+	                var source = X.responseText;
+	                // fix (if saved form for Chrome Dev Tools)
+	                if (source.substr(0,10)==="(function("){ 
+	                    var moduleStart = source.indexOf('{');
+	                    var moduleEnd = source.lastIndexOf('})');
+	                    var CDTcomment = source.indexOf('//@ ');
+	                    if (CDTcomment>-1 && CDTcomment<moduleStart+6) moduleStart = source.indexOf('\n',CDTcomment);
+	                    source = source.slice(moduleStart+1,moduleEnd-1); 
+	                } 
+	                // fix, add comment to show source on Chrome Dev Tools
+	                source="//@ sourceURL="+window.location.origin+url+"\n" + source;
+	                //------
+	                var module = { id: url, uri: url, exports:exports }; //according to node.js modules 
+	                var anonFn = new Function("require", "exports", "module", source); //create a Fn with module code, and 3 params: require, exports & module
+	                anonFn(require, exports, module); // call the Fn, Execute the module
+	                require.cache[url]  = exports = module.exports; //cache obj exported by module
+	            } catch (err) {
+	                throw new Error("Error loading module "+url+": "+err);
+	            }
+	    }
+	    return exports; //require returns object exported by module
+	}
+	
+	///- END REQUIRE FN
 async function xor() {
-    const SVM = await
-    require('https://cdn.jsdelivr.net/npm/libsvm-js@0.2.1/dist/browser/wasm/libsvm.js'); //getting the link to download
+    const SVM = await require('https://cdn.jsdelivr.net/npm/libsvm-js@0.2.1/dist/browser/wasm/libsvm.js'); //getting the link to download
     const svm = new SVM({
         kernel: SVM.KERNEL_TYPES.RBF, // The type of kernel I want to use
         type: SVM.SVM_TYPES.C_SVC,    // The type of SVM I want to run
-        gamma: 1,                     // RBF kernel gamma parameter
+        gamma: 0.001,                     // RBF kernel gamma parameter
         cost: 1                       // C_SVC cost parameter
     });
 
@@ -23,7 +60,7 @@ async function xor() {
     console.log(predictedLabel) // 0
 }
 
-xor().then(() => console.log('done!'));*/
+xor().then(() => console.log('done!'));
 //Vainavi's code
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var video = document.querySelector("#videoElement");  // gets tag from html page
@@ -87,41 +124,4 @@ if (navigator.mediaDevices.getUserMedia){  //checking to see if method exists
 		};
 		video.src = path;
 	}
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	///- REQUIRE FN
-	// equivalent to require from node.js
-	function require(url){
-	    if (url.toLowerCase().substr(-3)!=='.js') url+='.js'; // to allow loading without js suffix;
-	    if (!require.cache) require.cache=[]; //init cache
-	    var exports=require.cache[url]; //get from cache
-	    if (!exports) { //not cached
-	            try {
-	                exports={};
-	                var X=new XMLHttpRequest();
-	                X.open("GET", url, 0); // sync
-	                X.send();
-	                if (X.status && X.status !== 200)  throw new Error(X.statusText);
-	                var source = X.responseText;
-	                // fix (if saved form for Chrome Dev Tools)
-	                if (source.substr(0,10)==="(function("){ 
-	                    var moduleStart = source.indexOf('{');
-	                    var moduleEnd = source.lastIndexOf('})');
-	                    var CDTcomment = source.indexOf('//@ ');
-	                    if (CDTcomment>-1 && CDTcomment<moduleStart+6) moduleStart = source.indexOf('\n',CDTcomment);
-	                    source = source.slice(moduleStart+1,moduleEnd-1); 
-	                } 
-	                // fix, add comment to show source on Chrome Dev Tools
-	                source="//@ sourceURL="+window.location.origin+url+"\n" + source;
-	                //------
-	                var module = { id: url, uri: url, exports:exports }; //according to node.js modules 
-	                var anonFn = new Function("require", "exports", "module", source); //create a Fn with module code, and 3 params: require, exports & module
-	                anonFn(require, exports, module); // call the Fn, Execute the module
-	                require.cache[url]  = exports = module.exports; //cache obj exported by module
-	            } catch (err) {
-	                throw new Error("Error loading module "+url+": "+err);
-	            }
-	    }
-	    return exports; //require returns object exported by module
-	}
 	
-	///- END REQUIRE FN
